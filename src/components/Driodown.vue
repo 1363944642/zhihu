6e668cc1c9b36de7df3b042cb27eb75c2f3c315e
mil <1363944642@qq.com>
2020-11-27 03:11:52 +0800

    useClickOutside封装自定义函数

--- src/components/Driodown.vue
+++ src/components/Driodown.vue
@@ -14,8 +14,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, onMounted, onUnmounted } from 'vue'
-
+import { defineComponent, ref, watch, onMounted, onUnmounted } from 'vue'
+import useClickOutside from '../hooks/useClickOutside'
 export default defineComponent({
   name: 'Dropdown',
   props: {
@@ -30,19 +30,14 @@ export default defineComponent({
     const toggleOpen = () => {
       isOpen.value = !isOpen.value
     }
-    const handler = (e: MouseEvent) => {
-      if (dropdownRef.value) {
-        if (!dropdownRef.value.contains(e.target as HTMLElement) && isOpen.value) {
-          isOpen.value = false
-        }
+    const isClickOutside = useClickOutside(dropdownRef)
+
+    watch(isClickOutside, () => {
+      if (isOpen.value && isClickOutside.value) {
+        isOpen.value = false
       }
-    }
-    onMounted(() => {
-      document.addEventListener('click', handler)
     })
-    onUnmounted(() => {
-      document.removeEventListener('click', handler)
-    })
+
     return {
       isOpen,
       toggleOpen,
