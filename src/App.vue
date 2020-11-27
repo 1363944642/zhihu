3dd7822182654a99bb85c01482d40f7c8c411fb0
mil <1363944642@qq.com>
2020-11-27 21:16:07 +0800

    VailidateInput 简单实现

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <template>
   <div class="container">
     <Golbal-header :user="currentUser"></Golbal-header>
-    <ColumnList :list="list"></ColumnList>
+    <!-- <ColumnList :list="list"></ColumnList> -->
+    <form action="">
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">邮箱地址</label>
+        <input
+          type="email"
+          class="form-control"
+          id="exampleInputEmail1"
+          v-model="emailRef.val"
+          @blur="validateEmail"
+        />
+        <div class="form-text" v-if="emailRef.error">
+          {{ emailRef.message }}
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">密码</label>
+        <input
+          type="password"
+          class="form-control"
+          id="exampleInputPassword1"
+        />
+      </div>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+
+import { defineComponent, reactive } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
 import GolbalHeader, { UserProps } from './components/GolbalHeader.vue'
+
 const currentUser: UserProps = {
   isLogin: true,
   name: 'mil'
@@ -71,16 +96,34 @@ const testData: ColumnProps[] = [
   }
 ]
 
+const emailReg = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList,
+    // ColumnList,
     GolbalHeader
   },
   setup() {
+    const emailRef = reactive({
+      val: '',
+      error: false,
+      message: ''
+    })
+    const validateEmail = () => {
+      if (emailRef.val.trim() === '') {
+        emailRef.error = true
+        emailRef.message = '您没有输入邮箱'
+      } else if (!emailReg.test(emailRef.val)) {
+        emailRef.error = true
+        emailRef.message = '邮箱格式错误'
+      }
+    }
     return {
       list: testData,
-      currentUser
+      currentUser,
+      emailRef,
+      validateEmail
     }
   }
 })
