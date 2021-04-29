cbb294085a9dc31fa693f453a4b281fa4c9b6ca1
mil <1363944642@qq.com>
2021-04-29 20:19:31 +0800

    更新icode

--- src/main.ts
+++ src/main.ts
@@ -13,19 +13,17 @@ const app = createApp(App)
  */
 // 替换 baseURL
 axios.defaults.baseURL = 'http://apis.imooc.com/api/'
-// EF46985D7C85D229
-// 57951175B9EC3618
-// E47D035F540B0CD9
+
 axios.interceptors.request.use(config => {
   // git请求, icode添加到 url参数中
-  config.params = { ...config.params, icode: 'EF46985D7C85D229' }
+  config.params = { ...config.params, icode: '45391F6ED6885EF3' }
   // 其他请求, 添加到body中
   // 如果是上传文件,添加到FromData中
   if (config.data instanceof FormData) {
-    config.data.append('icode', 'EF46985D7C85D229')
+    config.data.append('icode', '45391F6ED6885EF3')
   } else {
     // 普通的body对象,添加到bata中
-    config.data = { ...config.data, icode: 'EF46985D7C85D229' }
+    config.data = { ...config.data, icode: '45391F6ED6885EF3' }
   }
   return config
 })
